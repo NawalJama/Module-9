7801278e40d6fb56a83d8bf5aafd2a8210035c63
Nawal Jama <nawaljng@gmail.com>
2023-05-27 07:32:54 -0400

    added questions into inquirer

--- develop/index.js
+++ develop/index.js
@@ -1,17 +1,36 @@
 // TODO: Include packages needed for this application
-const inquirer=require("inquirer");
-const path=require("path");
-const fs=require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's title?",
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What is the description of your project?",
+
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "How to install your application?",
+
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
