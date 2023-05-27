5609d3c4ec4ec910e8966205981281b438a33780
Nawal Jama <nawaljng@gmail.com>
2023-05-27 07:41:27 -0400

    added more questions for inquirer

--- develop/index.js
+++ develop/index.js
@@ -10,19 +10,47 @@ const questions = [
         type: 'input',
         name: 'title',
         message: "What is your project's title?",
-
     },
     {
         type: 'input',
         name: 'description',
         message: "What is the description of your project?",
-
     },
     {
         type: 'input',
         name: 'installation',
         message: "How to install your application?",
-
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Provide usage instructions for your project.",
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "What is the test instructions for your project?",
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Choose a license for your project.",
+        choices: ['MIT', 'APACHE_2.0', 'GPL_3.0', 'BSD_3', 'None' ]
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "How to contribute your project?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email?",
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is your github username?",
     },
 ];
 
